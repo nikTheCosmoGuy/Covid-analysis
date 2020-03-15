573b7f2e31182164163a6cee3f5d3c8e030ebca1
Nikolas Iwanus <nikTheCosmoGuy@users.noreply.github.com>
2020-03-16 00:13:41 +1100

    Initial commit

---
+++ main.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 15 09:14:18 2020
+
+@author: Nik
+"""
+
+import matplotlib.pyplot as plt
+import pandas as pd
+from pathlib import Path
+import datetime
+import os
+import numpy as np
+
+from scipy.stats import linregress
+
+today = datetime.date.today()
+
+
+def create_plot(df, week_marks=True, **kwargs):
+    
+    fig, ax = plt.subplots()
+    
+    df.plot(ax=ax, **kwargs)
+    
+    if week_marks:
+        for date in focus.index.array[6::7]:
+            date = pd.Timestamp(date)
+            ax.axvline(x=date, color='k',
+                       linestyle='--',
+                       alpha=0.5,
+                       lw=0.5)
+    
+    return fig, ax
+
+def fittable(df, least_cases=1):
+    return df.iloc[np.where(df>=1)[0]]
+
+def create_plotBK(df, week_marks=True, month_mark=False):
+    
+    pass
+
+def get_save_loc(today):
+    save_place = Path(os.path.join('results', str(today)))
+    save_place.absolute().mkdir(parents=True, exist_ok=True)
+    return save_place
+
+def day_name(date):
+    
+    day = today.weekday()
+    names = {0: 'Monday',
+             1: 'Tuesday',
+             2: 'Wednesday',
+             3: 'Thursday',
+             4: 'Friday',
+             5: 'Satday',
+             6: 'Sunday'}
+    return names[day]
+
+def make_folder(path):
+    
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+def growth(df):
+    return df-df.shift(1)
+
+def growth_rate(df):
+    return growth(df)/df.shift(1)
+
+def growth_factor(df):
+    return growth(df) / growth(df).shift(1)
+
+def country(df, cntry, states=False):
+    country = df.xs(cntry, level=1)
+    if not states:
+   
+        country = pd.DataFrame({cntry: country.sum()})
+
+    country.index.name = 'Date'
+
+    return country
+
+
+
+cntry = 'China'
+
+plt.close('all')
+plt.ion()
+
+loc = r'C:\RWA\Code\COVID-19\csse_covid_19_data\csse_covid_19_time_series'
+loc = Path(loc)
+
+state = 'Confirmed'
+
+data = pd.read_csv(loc.joinpath(f'time_series_19-covid-{state}.csv'))
+
+df = data.set_index(list(data.columns[0:2])).drop(['Lat', 'Long'], axis=1)
+df.columns = pd.to_datetime(df.columns, infer_datetime_format=True)
+
+total = df.sum().reset_index().rename(columns={'index': 'Date', 0: 'Total'})
+
+total = total.set_index('Date')
+#ax = total.plot()
+
+focus = country(df, cntry)
+
+#plt.figure()
+#ax2 = focus.plot()
+#fig = ax2.figure
+
+
+#plt.ioff()
+
+
+
+sub_focus = fittable(focus)
+
+
+country_list = data['Country/Region'].unique()
+
+fittable(sub_focus).plot(logy=True)
+
+fig3, ax3 = create_plot(sub_focus, logy=True)
+
+
+slope, intercept, rvalue, pvalue, stderr = linregress(x=np.array(range(len(sub_focus))),
+                                                      y=np.log(fittable(focus)).squeeze(),
+                                                      )
+
